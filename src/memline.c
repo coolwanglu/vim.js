075162e6e19a50fb23b5657e27be6fe7aaff014e
Lu Wang <coolwanglu@gmail.com>
2013-12-01 21:58:52 +0800

    auto commit by fix.py Fix conflict declaration: memline.c 245 15 ml_find_line

--- src/memline.c
+++ src/memline.c
@@ -243,7 +243,7 @@ static char_u *findswapname __ARGS((buf_T *, char_u **, char_u *));
 static void ml_flush_line __ARGS((buf_T *));
 static bhdr_T *ml_new_data __ARGS((memfile_T *, int, int));
 static bhdr_T *ml_new_ptr __ARGS((memfile_T *));
-static bhdr_T *ml_find_line __ARGS((buf_T *, linenr_T, int));
+static bhdr_T *ml_find_line __ARGS((buf_T *, linenr_T, int DECL_ASYNC_ARG));
 static int ml_add_stack __ARGS((buf_T *));
 static void ml_lineadd __ARGS((buf_T *, int));
 static int b0_magic_wrong __ARGS((ZERO_BL *));
