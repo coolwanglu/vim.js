c54e2df8eeb57698a6529d1df0eaac51bc1fc8cf
Lu Wang <coolwanglu@gmail.com>
2013-12-01 21:25:19 +0800

    auto commit by fix.py Insert call arg: memline.c 1458 27 5

--- src/memline.c
+++ src/memline.c
@@ -1456,7 +1456,7 @@ ml_recover( ASYNC_ARG_ONLY)
 		    {
 			++error;
 			ml_append(lnum++, (char_u *)_("???LINE COUNT WRONG"),
-							    (colnr_T)0, TRUE);
+							    (colnr_T)0, TRUE ASYNC_ARG);
 		    }
 		}
 
