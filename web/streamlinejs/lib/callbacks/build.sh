06622d0969f1336c59ecaf5e2c6a00f9e5a20d8d
Lu Wang <coolwanglu@gmail.com>
2013-12-31 23:00:13 +0800

    ..

--- web/streamlinejs/lib/callbacks/build.sh
+++ web/streamlinejs/lib/callbacks/build.sh
@@ -1,2 +1,2 @@
 cat require-stub.js runtime.js > streamline.js
-java -jar ~/src/closure-compiler/compiler.jar  --js streamline.js --js_output_file streamline.min.js --language_in ECMASCRIPT5
+java -jar $EM_DIR/third_party/closure-compiler/compiler.jar  --js streamline.js --js_output_file streamline.min.js --language_in ECMASCRIPT5
